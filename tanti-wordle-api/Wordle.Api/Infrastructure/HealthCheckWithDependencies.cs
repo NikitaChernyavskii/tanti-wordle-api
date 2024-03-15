d552b94ff98d8fbc4637a8cf25748e93aad1e818
Mikita_Charniauski <tantik1992@gmail.com>
2024-03-15 14:22:18 +0300

    infrastructure changes

---
+++ tanti-wordle-api/Wordle.Api/Infrastructure/HealthCheckWithDependencies.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Wordle.Api.Infrastructure
+{
+    [ExcludeFromCodeCoverage]
+    public class HealthCheckWithDependencies : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = true;
+            StringBuilder unhealthyDependecies = new StringBuilder();
+
+            // check dependencies here: SQL connections, apis, etc.
+
+            if (!isHealthy)
+            {
+                var dependencies = unhealthyDependecies.ToString();
+                return Task.FromResult(HealthCheckResult.Unhealthy($"Unhealthy dependencies: {dependencies}."));
+            }
+
+            return Task.FromResult(HealthCheckResult.Healthy("All dependencies are healthy."));
+        }
+    }
+}
